743ad79cdfad0edc56a17132278a8eeb2f8723ab
metaYogeshbhutkar <yogesh.bhutkar@m0.ventures>
2024-03-01 12:57:19 +0530

    Testing bot embedding feature

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div
+        style={{
+          borderRadius: "100%",
+          width: "100px",
+          height: "100px",
+          backgroundColor: "red",
+          flex: 1,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        Chat
+      </div>
+    </>
+  );
+}
+
+export default App;
